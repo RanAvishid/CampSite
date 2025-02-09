b205cbc6d98042f2d7739f70e24ad1feacd6a2ea
Ran Avishid <Ranavishid26@gmail.com>
2025-02-09 12:28:05 +0200

    Controlles and star ratings & Maps & Styles Clean up &  Deploying

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+    require ('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -9,13 +13,16 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./moudles/user');
-
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize')
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
 
+const MongoDBStore = require("connect-mongo")(session);
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console,"connection error:"));
@@ -32,19 +39,37 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+const secret = process.env.SECRET || 'thisshouldbeabettersecret';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSTION STORE ERROR",err)
+})
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 100 * 60 * 60 * 24 * 7,
         makeAge: 100 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -59,6 +84,8 @@ app.use((req,res,next) => {
     res.locals.error = req.flash('error');
     next();
 })
+
+
 
 
 app.use('/',userRoutes);
--- seeds/index.js
+++ seeds/index.js
@@ -16,16 +16,34 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i=0; i<50; i++){
+    for (let i=0; i<300; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random()*20) +10;
         const camp = new Campground ({
+            //YOUR USER ID
             author: '6789528e86b75e697a6efa9e',
             location: `${cities[random1000].city} , ${cities[random1000].state} `,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
-            description: 'blah blah blah blah blah blah',
-            price
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione veritatis rem beatae nesciunt dolorum odio error nostrum voluptas quasi maxime. Reprehenderit recusandae placeat aperiam vitae dolor iure illum voluptatibus nihil.',
+            price,
+            geometry:{
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+                
+            },
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dmbptxtix/image/upload/v1738087584/YelpCamp/qb3apyz4b2brc9uqv4pj.jpg',
+                  filename: 'YelpCamp/qb3apyz4b2brc9uqv4pj',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dmbptxtix/image/upload/v1738087584/YelpCamp/alqvmzroinqblvppislr.jpg',
+                  filename: 'YelpCamp/alqvmzroinqblvppislr',
+                },
+              ],
 
         })
         await camp.save();
